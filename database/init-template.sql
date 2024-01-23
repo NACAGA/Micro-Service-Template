8697f3610b9f6e2669e0ebe8cdc69afd72ace351
Calvin Kinateder <calvinkinateder@gmail.com>
2024-01-22 20:12:39 -0500

    drop table

--- database/init-template.sql
+++ database/init-template.sql
@@ -1,14 +1,3 @@
-CREATE TABLE Users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    status ENUM('ACTIVE', 'INACTIVE', 'BLOCKED') NOT NULL,
-    firstname VARCHAR(255),
-    lastname VARCHAR(255),
-    phone VARCHAR(255),
-    favoritecolor VARCHAR(255)
-);
-
 CREATE TABLE People (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
